c234382c6a2ef959bff492202136002c00104770
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-06-11 03:18:35 -0700

    Time: 30 ms (97.53%), Space: 16.8 MB (30.54%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,13 +1,15 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        l1=[]
-        l2=[]
+        count=[0] * (max(arr1)+1)
         for i in arr1:
-            if i not in arr2:
-                l2.append(i)
-            else:
-                l1.append(i)
-        arr_map={val: idx for idx, val in enumerate(arr2)}
-        l1.sort(key = lambda x: arr_map[x])
-        l2.sort()
-        return l1+l2
+            count[i]+=1
+        res=[]
+        for i in arr2:
+            while count[i]>0:
+                res.append(i)
+                count[i]-=1
+        for i in range(max(arr1)+1):
+            while count[i]>0:
+                res.append(i)
+                count[i]-=1
+        return res
