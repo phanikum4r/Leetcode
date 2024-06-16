73e6ac61f42118e7cbd5b68382fc51a25e58cc82
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-06-16 03:11:14 -0700

    Time: 572 ms (82.66%), Space: 30.7 MB (73.07%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.py
@@ -2,10 +2,9 @@ from collections import Counter
 from math import ceil
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        count=Counter(nums)
-        res=0
-        for val in count.values():
+        c=0
+        for val in Counter(nums).values():
             if val==1:
                 return -1
-            res+= ceil(val/3) 
-        return res
+            c+= ceil(val/3) 
+        return c
