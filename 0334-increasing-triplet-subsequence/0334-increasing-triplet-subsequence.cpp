db735737c0817d23cfd7c9f5d740c7a3ac20a5ca
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-06-18 19:45:28 -0700

    Time: 93 ms (25.59%), Space: 114.2 MB (65.24%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int l,r,n=nums.size();
+        if(n<3){
+            return false;
+        }
+        l=INT_MAX;
+        r=INT_MAX;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]<=l){
+                l=nums[i];
+            }
+            else if(nums[i]<=r){
+                r=nums[i];
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
