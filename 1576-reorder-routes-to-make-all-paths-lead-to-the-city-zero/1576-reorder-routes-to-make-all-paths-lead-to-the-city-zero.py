c6413e45fd76acb12de042028f30fec05d5bae19
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-07-07 02:23:43 -0700

    Time: 880 ms (87.09%), Space: 45.8 MB (71.15%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,20 +1,17 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph=defaultdict(list)
-        directed=set()
-        for a,b in connections:
-            graph[a].append(b)
-            graph[b].append(a)
-            directed.add((a,b))
+        graph=defaultdict(dict)
+        for a, b in connections:
+            graph[a][b] = 1
+            graph[b][a] = 0
         res=0
         visited=[False]*n
         q=deque([0])
         while q:
             city=q.popleft()
             visited[city]=True
-            for neighbour in graph[city]:
+            for neighbour,reverse in graph[city].items():
                 if not visited[neighbour]:
-                    if (city,neighbour) in directed:
-                        res+=1
+                    res+=reverse
                     q.append(neighbour)
         return res
