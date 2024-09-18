28221f7467544eb6f5bd77d474d3d399c73878d4
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-09-17 20:22:19 -0700

    Time: 48 ms (69.32%), Space: 16.5 MB (58.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -3,7 +3,6 @@ class Solution:
         nr=len(grid)
         nc=len(grid[0])
         directions=[(-1,0),(0,-1),(1,0),(0,1)]
-        result=0
         count=0
         v=0
         q=deque()             
@@ -15,13 +14,9 @@ class Solution:
                     count+=1
         while q:
             x,y,v=q.popleft()
-            result=max(result,v)
             for (r,c) in directions:
                 if 0<=x+r<nr and 0<=y+c<nc and grid[x+r][y+c]==1:
                     grid[x+r][y+c]=0
                     count-=1
                     q.append((x+r,y+c,v+1))
-                if 0<x+r<nr and 0<y+c<nc and grid[x+r][y+c]==2:
-                    grid[x+r][y+c]=0
-                    q.append((x+r,y+c,0))
-        return -1 if count else result
+        return -1 if count else v
