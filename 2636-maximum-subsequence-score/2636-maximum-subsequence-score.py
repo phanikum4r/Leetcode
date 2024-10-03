81a3740758869c43be2b366b8017e211fdd44ac9
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-03 00:52:58 -0700

    Time: 725 ms (96.36%), Space: 37.1 MB (85.24%) - LeetHub

---
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        arr=[(nums1[i],nums2[i]) for i in range(len(nums1))]
+        arr.sort(key=lambda x:x[1], reverse=True)
+        heap=[arr[i][0] for i in range(k)]
+        heapq.heapify(heap)
+        topsum=sum(heap)
+        res=topsum*arr[k-1][1]
+        for i in range(k,len(nums1)):
+            topsum-=heapq.heappop(heap)
+            heapq.heappush(heap,arr[i][0])
+            topsum+=arr[i][0]
+            res=max(topsum*arr[i][1],res)
+        return res
