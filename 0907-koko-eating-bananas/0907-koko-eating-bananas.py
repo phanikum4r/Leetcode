add5039d43990319f31e582625d6d9bc1e8c4f5e
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-04 20:38:23 -0700

    Time: 237 ms (97.79%), Space: 18.2 MB (63.48%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -3,8 +3,7 @@ class Solution:
         low,high=1,max(piles)
         while low<high:
             mid=(low+high)//2
-            res=sum([ceil(pile/mid) for pile in piles])
-            if res<=h:
+            if sum([ceil(pile/mid) for pile in piles])<=h:
                 high = mid
             else:
                 low=mid+1
