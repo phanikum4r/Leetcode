f4919078aca9d5670db525b07afd6095e00395df
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-15 16:06:23 -0700

    Time: 248 ms (85.33%), Space: 64.3 MB (6.61%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,30 +1,40 @@
-import random
-class RandomizedSet:
-
+from random import choice
+class RandomizedSet():
     def __init__(self):
-        self.s=[]
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
+
+        
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
         
 
-    def insert(self, val: int) -> bool:
-        if val not in self.s:
-            self.s.append(val)
-            return True
-        else:
-            return False
-
     def remove(self, val: int) -> bool:
-        try:
-            self.s.remove(val)
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete the last element
+            self.list.pop()
+            del self.dict[val]
             return True
-        except:
-            return False
+        return False
 
     def getRandom(self) -> int:
-        return self.s[random.randint(0,len(self.s)-1)]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)
