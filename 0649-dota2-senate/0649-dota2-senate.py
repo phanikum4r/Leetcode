726f474714fa7a08e1254852ba3ca8d5bb2141a8
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-15 16:38:06 -0700

    Time: 51 ms (55.07%), Space: 16.7 MB (88.28%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,26 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        dcount=senate.count('D')
+        rcount=len(senate)-dcount
+        rban=0
+        dban=0
+        q=deque(senate)
+        while q:
+            if dcount<=0:
+                return 'Radiant'
+            if rcount<=0:
+                return 'Dire'
+            if q.popleft()=='R':
+                if rban:
+                    rban-=1
+                    rcount-=1
+                else:
+                    dban+=1
+                    q.append('R')
+            else:
+                if dban:
+                    dban-=1
+                    dcount-=1
+                else:
+                    rban+=1
+                    q.append('D')
