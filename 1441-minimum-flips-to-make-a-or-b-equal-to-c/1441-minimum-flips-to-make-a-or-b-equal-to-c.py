678b7fb87847e4e2587f4c553de869b23afd9576
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-15 18:04:24 -0700

    Time: 29 ms (88.7%), Space: 16.6 MB (15.31%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,3 +1,7 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
         return bin((a | b) ^ c).count('1') + bin(((a & b) | c) ^ c).count('1')
+
+        # if position has 1 in both a and b, and 0 in c, 
+        # we need to count one more time as we are replacing twice. 
+        # second count checks this scenario
