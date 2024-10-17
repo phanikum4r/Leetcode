1de77fab97ff940f14bebd572b821df830245e45
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-16 20:36:48 -0700

    Time: 118 ms (60.02%), Space: 18.4 MB (53.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,12 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack=[]
-        cur_vol=0
-        for i in range(len(height)):
-            while stack and height[stack[-1]]<=height[i]:
-                left = stack.pop()
-                if not stack:
-                    break
-                cur_vol += (min(height[i], height[stack[-1]])-height[left])*(i-stack[-1]-1) 
-            stack.append(i)
-        return cur_vol
+        # stack=[]
+        # cur_vol=0
+        # for i in range(len(height)):
+        #     while stack and height[stack[-1]]<=height[i]:
+        #         left = stack.pop()
+        #         if not stack:
+        #             break
+        #         #subtract the left from height as we added to ans in previous
+        #         cur_vol += (min(height[i], height[stack[-1]])-height[left])*(i-stack[-1]-1) 
+        #     stack.append(i)
+        # return cur_vol
+
+        left_max, right_max = 0, 0
+        left, right = 0, len(height)-1
+        ans=0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(height[left], left_max)
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(height[right], right_max)
+                ans += right_max - height[right]
+                right -=1
+        return ans
