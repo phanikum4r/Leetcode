a95538199df68a910af3f4eba2b2082ada4a6ca8
Gayathri Phani Kumar Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-10-20 17:31:38 -0700

    Time: 27 ms (99.38%), Space: 17.6 MB (9.31%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -11,25 +11,72 @@ class Solution:
         #     start += 1
         # return count
 
-        if len(s)==10000:
-            if 'ba' in words:
-                return []
-            return [i for i in range(5001)]
+        # if len(s)==10000:
+        #     if 'ba' in words:
+        #         return []
+        #     return [i for i in range(5001)]
+        # word_count = Counter(words)
+        # length = len(words[0])
+        # word_len = length * len(words)
+        # result = []
+        # start = 0
+        # for i in range(start, len(s) - word_len + 1):
+        #     words_used = 0
+        #     word_copy = copy.deepcopy(word_count)
+        #     for j in range(i, i + word_len - length + 1, length):
+        #         sub = s[j:j+length]
+        #         if word_copy[sub]:
+        #             word_copy[sub] -= 1
+        #             words_used += 1
+        #         else:
+        #             break
+        #     if words_used == len(words):
+        #         result.append(i)
+        # return result
+
+        if not s or not words:
+            return []
+        
+        word_length = len(words[0])
+        total_concat_length = word_length * len(words)
         word_count = Counter(words)
-        length = len(words[0])
-        word_len = length * len(words)
-        result = []
-        start = 0
-        for i in range(start, len(s) - word_len + 1):
-            words_used = 0
-            word_copy = copy.deepcopy(word_count)
-            for j in range(i, i + word_len - length + 1, length):
-                sub = s[j:j+length]
-                if word_copy[sub]:
-                    word_copy[sub] -= 1
-                    words_used += 1
+        
+        result_indices = []
+        
+        # Check for all possible starting points in the first `word_length` characters
+        for i in range(word_length):
+            left = i
+            seen_words = Counter()
+            count = 0  # To track the number of valid words we've seen in the current window
+            
+            # Slide through the string with steps of `word_length`
+            for right in range(i, len(s) - word_length + 1, word_length):
+                current_word = s[right:right + word_length]
+                
+                if current_word in word_count:
+                    seen_words[current_word] += 1
+                    count += 1
+                    
+                    # If we have seen more of `current_word` than required, shrink the window from the left
+                    while seen_words[current_word] > word_count[current_word]:
+                        left_word = s[left:left + word_length]
+                        seen_words[left_word] -= 1
+                        count -= 1
+                        left += word_length
+                    
+                    # If the window size matches the number of words, we have a valid concatenation
+                    if count == len(words):
+                        result_indices.append(left)
+                        
+                        # Move the window forward by removing the leftmost word
+                        left_word = s[left:left + word_length]
+                        seen_words[left_word] -= 1
+                        count -= 1
+                        left += word_length
                 else:
-                    break
-            if words_used == len(words):
-                result.append(i)
-        return result
+                    # If current_word is not a valid word, reset the window
+                    seen_words.clear()
+                    count = 0
+                    left = right + word_length
+        
+        return result_indices
