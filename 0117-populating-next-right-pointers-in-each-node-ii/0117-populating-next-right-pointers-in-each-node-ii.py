f209ae37c425d4789c21d5d4813fe851a0bd90e4
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-11-28 20:33:26 -0700

    Time: 38 ms (85.57%), Space: 18.3 MB (9.49%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -16,17 +16,13 @@ class Solution:
         while q:
             child_list = []
             cur_list = q.popleft()
-            for i in range(len(cur_list)-1):
-                cur_list[i].next = cur_list[i+1]
+            for i in range(len(cur_list)):
+                if i < len(cur_list) - 1:
+                    cur_list[i].next = cur_list[i+1]
                 if cur_list[i].left:
                     child_list.append(cur_list[i].left)
                 if cur_list[i].right:
                     child_list.append(cur_list[i].right)
-            cur_list[-1].next = None
-            if cur_list[-1].left:
-                child_list.append(cur_list[-1].left)
-            if cur_list[-1].right:
-                child_list.append(cur_list[-1].right)
             if child_list:
                 q.append(child_list)
         return root
