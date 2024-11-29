e6e3601d1ca47ebf7b9ead652c8c7762b3379a11
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-11-28 20:51:05 -0700

    Time: 34 ms (95.86%), Space: 18.5 MB (9.49%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -12,17 +12,40 @@ class Solution:
     def connect(self, root: 'Node') -> 'Node':
         if not root:
             return None
-        q = deque([[root]])
-        while q:
-            child_list = []
-            cur_list = q.popleft()
-            for i in range(len(cur_list)):
-                if i < len(cur_list) - 1:
-                    cur_list[i].next = cur_list[i+1]
-                if cur_list[i].left:
-                    child_list.append(cur_list[i].left)
-                if cur_list[i].right:
-                    child_list.append(cur_list[i].right)
-            if child_list:
-                q.append(child_list)
+        # using queue    
+        # q = deque([[root]])
+        # while q:
+        #     child_list = []
+        #     cur_list = q.popleft()
+        #     for i in range(len(cur_list)):
+        #         if i < len(cur_list) - 1:
+        #             cur_list[i].next = cur_list[i+1]
+        #         if cur_list[i].left:
+        #             child_list.append(cur_list[i].left)
+        #         if cur_list[i].right:
+        #             child_list.append(cur_list[i].right)
+        #     if child_list:
+        #         q.append(child_list)
+        # return root
+
+        # using pointers with constant space, leftmost for next level start, prev for linking
+        leftmost, prev = root, None
+        cur = root
+        while leftmost:
+            cur = leftmost
+            prev, leftmost = None, None
+            while cur:
+                if cur.left:
+                    if prev:
+                        prev.next = cur.left
+                    else:
+                        leftmost = cur.left
+                    prev = cur.left
+                if cur.right:
+                    if prev:
+                        prev.next = cur.right
+                    else:
+                        leftmost = cur.right
+                    prev = cur.right
+                cur = cur.next
         return root
