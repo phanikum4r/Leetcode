4d59973a1f7e619ba8d1348cdc7b29617e86f6ef
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-11-29 21:23:18 -0700

    Time: 0 ms (100%), Space: 17.5 MB (15.58%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,17 +8,26 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        result = []
-        q = deque([[root]])
-        while q:
-            cur = q.popleft()
-            new = []
-            result.append(cur[-1].val)
-            for node in cur:
-                if node.left:
-                    new.append(node.left)
-                if node.right:
-                    new.append(node.right)
-            if new:
-                q.append(new)
-        return result
+        # result = []
+        # q = deque([[root]])
+        # while q:
+        #     cur = q.popleft()
+        #     new = []
+        #     result.append(cur[-1].val)
+        #     for node in cur:
+        #         if node.left:
+        #             new.append(node.left)
+        #         if node.right:
+        #             new.append(node.right)
+        #     if new:
+        #         q.append(new)
+        # return result
+
+        self.result = {}
+        def trav(node, level):
+            if node:
+                self.result[level] = node.val
+                trav(node.left, level+1)
+                trav(node.right, level+1)
+        trav(root, 0)
+        return list(self.result.values())
