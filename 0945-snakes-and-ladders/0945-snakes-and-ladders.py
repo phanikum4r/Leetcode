5ad63f7cfb9e3c9955cf5769d8c74088c387dd95
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-12-04 00:54:22 -0700

    Time: 19 ms (72.98%), Space: 17.5 MB (5.88%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,9 +1,17 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        def findidx(cell):
-            quot, rem = divmod(cell-1, n)
-            return [n-quot-1,  n-rem-1] if quot%2 else [n-quot-1, rem]
+        # def findidx(cell):
+        #     quot, rem = divmod(cell-1, n)
+        #     return [n-quot-1,  n-rem-1] if quot%2 else [n-quot-1, rem]
+        cells = [None] * (n**2 + 1)
+        columns = list(range(0, n))
+        label = 1
+        for row in range(n - 1, -1, -1):
+            for column in columns:
+                cells[label] = [row, column]
+                label += 1
+            columns.reverse()
         visited = set()
         q = deque([[1,0]])
         while q:
@@ -11,7 +19,7 @@ class Solution:
             if cur == n**2:
                 return count
             for nextloc in range(cur+1, min(n**2, cur + 6) + 1):
-                r, c = findidx(nextloc)
+                r, c = cells[nextloc]
                 distance = board[r][c] if board[r][c]!=-1 else nextloc
                 if distance not in visited:
                     visited.add(distance)
