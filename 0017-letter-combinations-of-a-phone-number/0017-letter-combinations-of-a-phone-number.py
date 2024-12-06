ec0e4772abe6d094b56f8a50a01af5f028af67fe
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-12-05 17:38:06 -0700

    Time: 0 ms (100%), Space: 17.4 MB (5.23%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,17 +1,16 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        n=len(digits)
-        arr = []
-        res=[]
-        letter={"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz",}
-        def findsubsets(arr,index):
-            if index>=n:
-                return
-            for val in letter[digits[index]]:
-                arr.append(val)
-                if index==n-1:
-                    res.append("".join(arr))
-                findsubsets(arr,index+1)
-                arr.pop()
-        findsubsets(arr,0)
-        return res
+        if digits == "":
+            return []
+        phone = {'2': 'abc', '3': 'def', '4':'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        result = []
+        def backtrack(i, s):
+            for letter in phone[digits[i]]:
+                s.append(letter)
+                if len(s)==len(digits):
+                    result.append("".join(s))
+                else:
+                    backtrack(i+1, s)
+                s.pop()
+        backtrack(0, [])
+        return result
