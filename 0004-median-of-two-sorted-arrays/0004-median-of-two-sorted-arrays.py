6dc46797b85e5263c3a4bb2e1f4af0884349031a
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-12-10 21:54:36 -0700

    Time: 0 ms (100%), Space: 17.4 MB (19.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,36 +1,27 @@
 import math
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1=len(nums1)
-        n2=len(nums2)
-        if n1>n2:
-            return self.findMedianSortedArrays(nums2,nums1)
-        n=n1+n2
-        left=(n1+n2+1)//2
-        low=0
-        high=n1
-        while low<=high:
-            l1=float('-INF')
-            l2=float('-INF')
-            r1=float('INF')
-            r2=float('INF')
-            mid1=(low+high)//2
-            mid2=left-mid1
-            if mid1<n1:
-                r1=nums1[mid1]
-            if mid2<n2:
-                r2=nums2[mid2]
-            if mid1-1>=0:
-                l1=nums1[mid1-1]
-            if mid2-1>=0:
-                l2=nums2[mid2-1]
-            if l1<=r2 and l2<=r1:
-                if n%2==1:
-                    return max(l1,l2)
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partitionA = (left + right) // 2
+            partitionB = (m + n + 1) // 2 - partitionA
+
+            maxLeftA = float("-inf") if partitionA == 0 else nums1[partitionA - 1]
+            minRightA = float("inf") if partitionA == m else nums1[partitionA]
+            maxLeftB = float("-inf") if partitionB == 0 else nums2[partitionB - 1]
+            minRightB = float("inf") if partitionB == n else nums2[partitionB]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
                 else:
-                    return (max(l1,l2)+min(r1,r2))/2.0
-            elif l1>r2:
-                high=mid1-1
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partitionA - 1
             else:
-                low=mid1+1
-        return 0
+                left = partitionA + 1
