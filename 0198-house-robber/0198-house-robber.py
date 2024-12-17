319472ea06c8e203a81ff737fc4232a11dc27bdd
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2024-12-16 18:51:48 -0700

    Time: 0 ms (100%), Space: 17.2 MB (17.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -11,9 +11,16 @@ class Solution:
         #         return memo[index]
         # return dp(len(nums)-1)
 
+
         if len(nums)==1:
             return nums[0]
         nums[1]=max(nums[0],nums[1])
         for i in range(2,len(nums)):
                 nums[i] = max(nums[i]+nums[i-2],nums[i-1])
         return nums[-1]
+
+
+        # a, b = nums[0], nums[1]
+        # for i in range(2, len(nums)):
+        #     a, b = max(a, b), max(nums[i] + a, b)
+        # return max(a, b)
