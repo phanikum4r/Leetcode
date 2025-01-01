6a359e151fbfd7d97f41cd3e5e7c537d7424ae7f
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-01 14:06:25 -0700

    Time: 191 ms (94.38%), Space: 17.7 MB (36.64%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        m,i,j=0,0,0
-        low=0
-        high=len(s)
-        while low<high-1:
-            for (a,b) in [(low,low),(low,low+1)]:
-                c=0
-                while a>=0 and b<high and s[a]==s[b]:
-                    a-=1
-                    b+=1
-                    if a+1==b-1:
-                        c+=1
-                    else:
-                        c+=2  
-                if c>m:
-                    m=c
-                    i=a+1
-                    j=b-1
-            low+=1
-        return s[i:j+1]
-                    
+        # using dp 2d array 
+        # n = len(s)
+        # start, end = 0, 0
+        # dp = [[0] * n for _ in range(n)]
+        # for i in range(n-1, -1, -1):
+        #     for j in range(i + 1, n):
+        #         dp[i][j] = (s[i] == s[j]) and (j-i <= 2  or dp[i+1][j-1])
+        #         if dp[i][j] and (j - i > end - start):
+        #             start, end = i, j
+        # return s[start: end + 1]
+
+        # expanding from centers O(n**2) O(1)
+        n = len(s)
+        start, end = 0, 0
+        for i in range(n):
+            for j in (0, 1):
+                left, right = i, i+j
+                while left>=0 and right<n and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                left += 1
+                right -=1
+                if right - left > end - start:
+                    start, end = left, right
+        return s[start: end + 1]
