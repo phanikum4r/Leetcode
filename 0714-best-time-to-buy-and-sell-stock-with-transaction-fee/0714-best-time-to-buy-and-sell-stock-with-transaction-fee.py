ba415ca3e4f630710956ea98e0bfedaa67962635
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-02 01:05:32 -0700

    Time: 94 ms (82.12%), Space: 23.5 MB (41.15%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,6 +2,8 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         cash, hold = 0, -prices[0]
         for i in range(1, len(prices)):
+            # cash can be from previous cash or sell now and get cash
             cash = max(cash, hold + prices[i] - fee)
+            # hold from previous hold or buy with existing cash
             hold = max(hold, cash - prices[i])
         return cash
