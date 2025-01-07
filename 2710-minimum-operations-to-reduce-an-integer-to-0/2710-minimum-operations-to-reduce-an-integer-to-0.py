27c557c35d1906344458a6ff82bcecd310673e32
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-07 14:34:38 -0700

    Time: 0 ms (100%), Space: 18.1 MB (5.71%) - LeetHub

---
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        res = 0
+        while n > 0:
+            if n % 2 == 0:
+                n >>= 1
+            elif (n & 2) > 0:
+                n += 1
+                res += 1
+            else:
+                res += 1
+                n >>= 2
+        return res
+        
+        # return (n ^ (n * 3)).bit_count()
