f8adb307171ff8b6aa72d9bb70d098b1a81ad925
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-07 02:03:51 -0700

    Time: 79 ms (93.95%), Space: 36 MB (66.89%) - LeetHub

--- 2483-task-scheduler-ii/2483-task-scheduler-ii.py
+++ 2483-task-scheduler-ii/2483-task-scheduler-ii.py
@@ -5,10 +5,12 @@ class Solution:
         for task in tasks:
             day = last.get(task, -1)
             days_passed = days - day
-            if day == -1 or space <= days_passed:
-                last[task] = days + 1
+            # task never encountered or cooldown completed
+            if day == -1 or days_passed >= space:
                 days += 1
+                last[task] = days
             else:
+                # calculate remaining days to wait
                 days += space - days_passed + 1
                 last[task] = days
         return days
