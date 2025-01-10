0717a8f3dfb60f8dc1e89f941f1b6d6c2d38a27d
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-09 18:46:45 -0700

    Time: 94 ms (97.37%), Space: 33.9 MB (18.29%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,28 +1,19 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        maxArea = 0
-        dp = [[0, 0, 0] for _ in range(n)]
-        for j in range(n):
-            if matrix[0][j] == '1':
-                dp[j] = [1, 1, 1]
-                maxArea = 1
-        for i in range(1, m):
+        side = 0
+        dp = [0 for _ in range(n)]
+        for i in range(m):
             prev = dp[:]
-            dp = [[0, 0, 0] for _ in range(n)]
+            dp = [0 for _ in range(n)]
             for j in range(n):
                 if matrix[i][j] == '1':
-                    if j == 0:
-                        dp[j] = [1, 1, 1]
-                        maxArea = max(maxArea, 1)
-                        continue
-                    dp[j][1] += dp[j - 1][1] + 1
-                    dp[j][2] += prev[j][2] + 1
-                    diag = int(sqrt(prev[j-1][0]))
-                    side = min(dp[j][1], dp[j][2], diag + 1)
-                    dp[j][0] = side * side
-                    if dp[j][0] > maxArea:
-                        maxArea = dp[j][0]
+                    if j == 0 or i == 0:
+                        dp[j] = 1
+                    else:
+                        dp[j] = min(dp[j-1], prev[j], prev[j-1]) + 1
+                    if dp[j] > side:
+                        side = dp[j]
                 else:
-                    dp[j] = [0, 0, 0]
-        return maxArea
+                    dp[j] = 0
+        return side * side
