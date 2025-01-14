fc82bbcdf7da85ecc8f82feec6e072ce7080d8be
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-01-13 23:36:31 -0700

    Time: 11 ms (76.65%), Space: 18.2 MB (39.99%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        arr=[['.' for j in range(n)] for i in range(n)]
+        result = []
+        cols, diagnols, antidiagnols = set(), set(), set()
+        def backtrack(row, cols, diagnols, antidiagnols, arr):
+            if row == n:
+                return
+            for col in range(n):
+                diagnol = row - col
+                anti = row + col
+
+                if col in cols or diagnol in diagnols or anti in antidiagnols:
+                    continue
+                
+                arr[row][col] = 'Q'
+                if row == n-1:
+                    temp = []
+                    for r in arr:
+                        temp.append(''.join(r))
+                    result.append(temp)
+                cols.add(col)
+                diagnols.add(diagnol)
+                antidiagnols.add(anti)
+                backtrack(row + 1, cols, diagnols, antidiagnols, arr)
+                cols.remove(col)
+                diagnols.remove(diagnol)
+                antidiagnols.remove(anti)
+                arr[row][col] = '.'
+        backtrack(0, cols, diagnols, antidiagnols, arr)
+        return result
